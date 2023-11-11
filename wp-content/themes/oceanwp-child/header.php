7efc729ac94e241670676a1aa4ac12c0dfd4a3a8
Abdel Ait Said <aaitsaid78@gmail.com>
2023-11-11 16:53:59 +0100

    Ajout de tous les fichiers wordpress modifiés ou non trackés

--- wp-content/themes/oceanwp-child/header.php
+++ wp-content/themes/oceanwp-child/header.php
@@ -12,35 +12,10 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="stylesheet" href="./oceanwp-child/style.css">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> <?php oceanwp_schema_markup('html'); ?>>
 
 	<header class="header">
-		<a href="http://localhost/Planty5/wordpress-6.3-fr_FR/wordpress">
-			<img class="logo"
-				src="http://localhost/Planty5/wordpress-6.3-fr_FR/wordpress/wp-content/uploads/2023/09/Logo-300x28.png"
-				alt="logo Planty">
-		</a>
-		<nav>
-			<ul class="liste-liens">
-				<li>
-					<a href="http://localhost/Planty5/wordpress-6.3-fr_FR/wordpress/nous-rencontrer/"
-						class="lien-rencontrer">Nous rencontrer</a>
-				</li>
-				<?php
-				if (is_user_logged_in()) {
-					// Condition pour vérifier si l'utilisateur est connecté à WordPress
-					echo '<li><a href="http://localhost/Planty5/wordpress-6.3-fr_FR/wordpress/wp-admin/" class="lien-admin">Admin</a></li>';
-				}
-				?>
-				<li>
-					<a href="http://localhost/Planty5/wordpress-6.3-fr_FR/wordpress/commander"
-						class="button">Commander</a>
-				</li>
-			</ul>
-		</nav>
-
-	</header>
